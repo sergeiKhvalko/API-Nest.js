0d99599b18e3da7177cbd02850d80b51ef1d9037
Sergei Khvalko <falko.serega2016@yandex.ru>
2022-09-02 17:12:52 +0300

    refactor: reupload app

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,26 +1,27 @@
-export enum topLevelCategory {
-  Courses,
-  Services,
-  Books,
-  Products
+export enum TopLevelCategory {
+	Courses,
+	Services,
+	Books,
+	Products,
 }
 
 export class TopPageModel {
-  firstCategory: topLevelCategory;
-  secondCategory: string;
-  title: string;
-  category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
-  seoText: string;
-  tagsTitle: string;
-  tags: string[];
+	_id: string;
+	firstCategory: TopLevelCategory;
+	secondCategory: string;
+	title: string;
+	category: string;
+	hh?: {
+		count: number;
+		juniorSalary: number;
+		middleSalary: number;
+		seniorSalary: number;
+	};
+	advantages: {
+		title: string;
+		description: string;
+	}[];
+	seoText: string;
+	tagsTitle: string;
+	tags: string[];
 }
